c6996cfa18849099fdbc6e6bd5c7ae57a87941ee
SaraHossameldin <sarahmfaheem@gmail.com>
2024-03-30 21:52:50 +0200

    Final without audio

--- gameover.cpp
+++ gameover.cpp
@@ -1,18 +1,12 @@
-#include "game_over.h"
-#include "Ui_game_over.h"
+#include "gameover.h"
 
 game_over::game_over(QWidget *parent)
     : QDialog(parent)
-    , ui(new Ui::game_over)
-{
-    ui->setupUi(this);
 
+{
     this->setVisible(false);
 }
 
-game_over::~game_over()
-{
-    delete ui;
-}
+
 
 
